6769f3ae702235a5f748d61085c2b8f73d82dc94
Mohammed Motara <mohammedmotara@Mohammeds-MacBook.local>
2019-06-29 15:21:41 +0100

    Created FA library

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import HomePage from './containers/homePageContainer';
+import AboutMe from './containers/aboutMePage/aboutMePageContainer';
+import NavigationBar from './components/navigationBar/navigationBar';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTwitterSquare } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faTwitterSquare);
 
 
 const App: React.FC = () => {
   return (
-    <HomePage />
+    <>
+      <NavigationBar />
+      <AboutMe />
+    </>
   );
 }
 
