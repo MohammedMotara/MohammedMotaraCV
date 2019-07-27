da1e89ba52ca18aff10133f3bedd5d5ed22b4176
Mohammed Motara <mohammedmotara@Mohammeds-MacBook.local>
2019-07-27 20:37:46 +0100

    added data file w/ hard coded data

---
+++ src/assets/HardCodedData/aboutMePageData.tsx
@@ -0,0 +1,14 @@
+
+
+export const servicesArray = [
+    "Start-Ups", 
+    "Data & Privacy", 
+    "Tech Policy & Regulation", 
+    "DevOps", 
+    "ML Models", 
+    "Front End Development"
+];
+
+
+
+
